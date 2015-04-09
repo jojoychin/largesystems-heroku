d92b6b354f0cdc2ef916fcba5d87f578be87283f
jojoychin <jochin@joannas-air.newschool.edu>
2015-04-08 20:13:07 -0400

    updated text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('This is totally working!!! Wohoo!');
+  res.send('This is Jo\'s first heroku test app');
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
